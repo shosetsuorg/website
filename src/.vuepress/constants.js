31cae38993c74e3aaa15a2b39983df8e0f8a585f
Doomsdayrs <38189170+Doomsdayrs@users.noreply.github.com>
2020-12-21 12:54:24 -0500

    Create constants.js
    Copied from tachiyomiorg/constants.js

---
+++ src/.vuepress/constants.js
@@ -0,0 +1,7 @@
+export const GITHUB_EXTENSION_JSON = "https://raw.githubusercontent.com/shosetsuorg/extensions/src/main/resources/index.json";
+
+export const GITHUB_STABLE_API = "https://api.github.com/repos/shosetsuorg/android-app/releases/latest";
+export const GITHUB_STABLE_RELEASE = "https://github.com/shosetsuorg/android-app/releases/latest";
+
+export const GITHUB_PREVIEW_API = "https://api.github.com/repos/shosetsuorg/shosetsu-preview/releases/latest";
+export const GITHUB_PREVIEW_RELEASE = "https://github.com/shosetsuorg/shosetsu-preview/releases/latest";
