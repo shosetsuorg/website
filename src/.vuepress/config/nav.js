04fe0fcc537f29503f1e0dde0458207546d28ce5
Doomsdayrs <38189170+Doomsdayrs@users.noreply.github.com>
2021-10-05 15:10:13 -0400

    Remove F-Droid Link
    F-Droid does not hold the most up to date version of shosetsu anymore. Will wait for V2 to be released to provide to F-Droid

--- src/.vuepress/config/nav.js
+++ src/.vuepress/config/nav.js
@@ -28,9 +28,5 @@ module.exports = [
 			}
 		]
 	},
-	{ text: "Discord", link: "https://discord.gg/ttSX7gB" },
-	{
-		text: "F-Droid",
-		link: "https://f-droid.org/packages/com.github.doomsdayrs.apps.shosetsu/"
-	}
+	{ text: "Discord", link: "https://discord.gg/ttSX7gB" }
 ];
