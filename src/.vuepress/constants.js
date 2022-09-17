e6236ced0b578f6a504815dc900e28f908c23862
Doomsdayrs <38189170+Doomsdayrs@users.noreply.github.com>
2022-09-17 01:53:18 -0400

    Use gitlab for downloading instead of github (#73)
    * Redirect stable to gitlab
    
    * Conform to gitlab download style

--- src/.vuepress/constants.js
+++ src/.vuepress/constants.js
@@ -1,7 +1,7 @@
 export const GITHUB_EXTENSION_JSON = "https://raw.githubusercontent.com/shosetsuorg/extensions/src/main/resources/index.json";
 
-export const GITHUB_STABLE_API = "https://api.github.com/repos/shosetsuorg/shosetsu/releases/latest";
-export const GITHUB_STABLE_RELEASE = "https://github.com/shosetsuorg/shosetsu/releases/latest";
+export const GITHUB_STABLE_API = "https://gitlab.com/api/v4/projects/39099987/releases";
+export const GITHUB_STABLE_RELEASE = "https://gitlab.com/shosetsuorg/shosetsu/-/releases/permalink/latest";
 
 export const GITHUB_PREVIEW_API = "https://api.github.com/repos/shosetsuorg/shosetsu-preview/releases/latest";
 export const GITHUB_PREVIEW_RELEASE = "https://github.com/shosetsuorg/shosetsu-preview/releases/latest";
